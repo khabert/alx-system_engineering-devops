1b8c061710870607786f2418a81052f93dba5105
khabert <khabertzion11@gmail.com>
2023-05-02 06:09:06 -0700

    school

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 1
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+string = ARGV[0]
+
+if string.match(/School/)
+  puts string1.chomp
+else
+  exit 1
+end
