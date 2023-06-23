08efe1ffd5c3e51d1f30b304599cb78a9d02d350
Khabert <you@example.com>
2023-06-23 12:21:56 -0700

    task two

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
-file { '/tmp/school':
-  ensure  => file,
+# Creates a file in /tmp
+
+file { 'school':
+  ensure  => 'present',
   content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
+  path    => '/tmp/school',
 }
